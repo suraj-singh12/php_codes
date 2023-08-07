cedd030fc12d64573d20f6f5dcfd4125c3aca597
suraj-singh12 <surajsingh5092@gmail.com>
2023-08-07 15:01:38 +0530

    refactoring

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <body>
 
@@ -7,22 +7,4 @@ echo "My first PHP script!";
 ?> 
 
 </body>
-</html> -->
-
-<?php
-$x = 7;
-{
-    $y = 10; echo $y ."\n";
-    {
-        $x = 25; echo $x ."\n";
-    }
-    while($y != 25) {
-        $x = 14; echo $x ."\n";
-        echo "I am inside the block\n";
-        break;
-    }
-    $x = 7;
-    echo $x ."\n"; echo $y;
-}
-
-
+</html>
---
+++ scope.php
@@ -0,0 +1,23 @@
+
+<?php
+const x = 5200;
+echo x ."\n";
+// will lead to an error
+// const x = 7;
+// echo x ."\n";
+{
+    // $y => syntax for creating variables
+    $y = 10; echo $y ."\n";
+    {
+        $x = 25; echo $x ."\n";
+    }
+    while($y != 25) {
+        $x = 14; echo $x ."\n";
+        echo "I am inside the block\n";
+        break;
+    }
+    $x = 7;
+    echo $x ."\n"; echo $y;
+}
+
+
